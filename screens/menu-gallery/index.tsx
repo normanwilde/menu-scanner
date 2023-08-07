e37268bd872b73804ea1cd9541847bf0ab5d073f
Norman Wilde <norman.wilde@dakai.io>
2023-08-07 23:27:57 +0200

    fix: check loading first not page.length

--- screens/menu-gallery/index.tsx
+++ screens/menu-gallery/index.tsx
@@ -5,15 +5,16 @@ import { useMenu } from '../../contexts/menu'
 export default function MenuGallery() {
   const { state } = useMenu()
 
-  if (state.pages && state.pages.length === 0) {
-    return <Text>No photos yet</Text>
-  }
   if (state.loading) {
     return <Text>Loading</Text>
   }
 
   if (state.error) {
     return <Text>Error</Text>
+  }
+
+  if (state.pages && state.pages.length === 0) {
+    return <Text>No photos yet</Text>
   }
 
   return (
